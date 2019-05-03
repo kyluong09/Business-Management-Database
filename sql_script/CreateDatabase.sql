86265b1a536d2282e0a3ca59b9d028a190f64571
kyluong09 <kyluong09@gmail.com>
2019-05-02 23:30:56 -0500

    added eer diagram folder

--- sql_script/CreateDatabase.sql
+++ sql_script/CreateDatabase.sql
@@ -34,6 +34,7 @@ PRIMARY KEY (`id`)
 
 )Engine=Innodb DEFAULT CHARSET=latin1;
 
+
 DROP TABLE IF EXISTS `Product`;
 
 CREATE TABLE `product`(
@@ -47,7 +48,7 @@ PRIMARY KEY(`id`)
 )Engine=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `customer_order`;
-CREATE TABLE `customer_order`(
+CREATE TABLE `order`(
 `id` int(40) NOT NULL AUTO_INCREMENT,
 `customer_id` int(40) DEFAULT NULL,
 
@@ -72,5 +73,42 @@ CONSTRAINT `FK_ORDER_ID` FOREIGN KEY(`order_id`) REFERENCES `order` (`id`) ON DE
 CONSTRAINT `FK_PRODUCT_ID` FOREIGN KEY(`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 
 )Engine=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`username` varchar(50) NOT NULL,
+`password` varchar(68) NOT NULL,
+`first_name` varchar(50) NOT NULL,
+`last_name` varchar(50) NOT NULL,
+`enabled` TinyInt(11) NOT NULL,
+PRIMARY KEY(`id`),
+UNIQUE KEY (`username`)
+)Engine=InnoDB DEFAULT CharSet=latin1;
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(50) DEFAULT NULL,
+
+PRIMARY KEY (`id`)
+
+)Engine=InnoDB DEFAULT CharSet=latin1;
+
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`(
+`user_id` int(11) NOT NULL,
+`role_id` int(11) NOT NULL,
+
+PRIMARY KEY(`user_id`,`role_id`),
+  KEY `FK_ROLE_idx` (`role_id`),
+FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)  ON DELETE NO ACTION ON UPDATE NO ACTION,
+FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)  ON DELETE NO ACTION ON UPDATE NO ACTION
+
+
+)Engine=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 SET FOREIGN_KEY_CHECKS = 1;
